bf942dc0ac5e76d948b6a78f1c5b7673eec2f5f6
rakibeabdelaboudou <rakibeabdelaboudou@gmail.com>
2024-01-15 11:33:01 +0100

    Release commit of dev

---
+++ app/Models/Frontsetting.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Frontsetting extends Model
+{
+    protected $table = 'front_setting';
+    protected $guarded = array();
+    public $timestamps = false;
+}
